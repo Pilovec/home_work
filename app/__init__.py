76d32d05d62a1f5d885b381bf401a007a6afe3c2
Pilovec <teperchuk-av@utg.ua>
2019-06-06 16:58:30 +0300

    Mega-tutorial Flask part XIII complite. Language (to Razmetka tekstow...)

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -10,6 +10,7 @@ from config import Config
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+babel = Babel(app)
+login.login_message = _l('Please log in to access this page.')
 #login.login_message = "Пожалуйста, войдите, чтобы открыть эту страницу."
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -50,5 +53,9 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import routes, models, errors
